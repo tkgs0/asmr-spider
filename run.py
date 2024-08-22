57a5d36e3b8afb8680cbabd44c084d6fe845872f
tkgs0 <hgc.sakuya@ya.ru>
2024-08-22 15:11:17 +0000

    :technologist:

---
+++ run.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+from asmr_spider import parser, dload, logger
+import sys, asyncio
+
+
+def main():
+    try:
+        args = parser.parse_args()
+        args.input = args.input or input("\033[33;1m请输入RJ号\033[0m: ").split()
+        asyncio.run(dload(args.input, args.action))
+    except KeyboardInterrupt:
+        logger.error("进程被手动终止.")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
