ddec0bd8f7258d2f0ee7a3d4a9e4fb25e5062710
tkgs0 <hgc.sakuya@ya.ru>
2024-08-29 09:32:29 +0000

    :art: :bug:

--- asmr_spider/__init__.py
+++ asmr_spider/__init__.py
@@ -1,11 +1,10 @@
 from .spider import ASMRSpider
 from .config import logger, progress
 from typing import List
-import argparse
-import shutil
+import argparse, shutil
 
 
-parser = argparse.ArgumentParser(description='Spide form asmr.one')
+parser = argparse.ArgumentParser(description='Spide for asmr.one')
 
 parser.add_argument(
     'input',
@@ -14,10 +13,11 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    '-a', '--action',
-    choices=['checksize', 'checktime', 'redownload', 'nocheck'],
+    '-a',
+    '--action',
+    choices=['checksize', 'checktime', 'redown', 'nocheck'],
     default='checksize',
-    help='是否检查已下载内容, checksize对比服务器文件大小,checktime, redownload重新下载, nocheck跳过已下载内容, 默认checksize'
+    help='checksize 对比文件大小; checktime 对比音频时长; redown 重新下载; nocheck 禁用检查; 默认为 checksize'
 )
 
 
@@ -34,12 +34,11 @@ async def dload(args: List[str], action):
 def check_ffmpeg_installed(is_need_check):
     if not is_need_check:
         return False
-    if shutil.which('ffmpeg') is not None and shutil.which('ffprobe') is not None:
-        logger.warning(f := f"FFMPEG and FFPROBE 启用, 增加支持的格式。")
-        progress.console.log(f)
+    if shutil.which('ffmpeg') and shutil.which('ffprobe'):
+        logger.success(e := f"检测到 FFMPEG 和 FFPROBE, 已增加支持校验的音频格式.")
+        progress.console.log(e, style='bold green on black')
         return True
     else:
-        logger.warning(
-            f := f"FFMPEG 或者 FFPROBE 没有检测到, 将仅支持 MP3、wav、flac 格式的音频.")
-        progress.console.log(f)
+        logger.warning(e := f"未检测到 FFMPEG 或 FFPROBE, 仅支持校验 MP3、wav、flac 格式的音频.")
+        progress.console.log(e, style='bold yellow on black')
         return False
